023c1287b942fb383d63e5d248e94b4f5d8361ef
Samuel Suther David <suther.david.samuel@gmail.com>
2023-10-01 10:26:53 +0800

    bad way of doing concurency with time.Sleep()

---
+++ first-example/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func printSomething(s string) {
+	fmt.Println(s)
+}
+
+func main() {
+
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"theta",
+		"epsilon",
+	}
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x))
+	}
+	time.Sleep(1 * time.Second)
+	printSomething("This is the second thing to be printed")
+}
