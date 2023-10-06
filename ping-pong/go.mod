6413156d91410d909abbe3248086f1075d390ccf
Samuel Suther David <suther.david.samuel@gmail.com>
2023-10-06 23:34:53 +0800

    ping pong example

---
+++ ping-pong/go.mod
@@ -0,0 +1,3 @@
+module ping_pong
+
+go 1.21.1
---
+++ ping-pong/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		s := <- ping
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+		fmt.Println("Executing loop")
+	}
+}
+
+func main() {
+
+	ping := make(chan string)
+	pong := make(chan string)
+
+	go shout(ping, pong)
+
+	fmt.Println("Type something and press Enter (enter Q to quit)")
+	
+	for {
+		// print a prompt
+		fmt.Print("-> ")
+
+		// get user input
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+
+		if strings.ToLower(userInput) == "q" {
+			break
+		}
+
+		ping <- userInput
+		// wait for response
+		response := <- pong
+		fmt.Println(response)
+	}
+	close(ping)
+	close(pong)
+	fmt.Println("End of the program")
+}
